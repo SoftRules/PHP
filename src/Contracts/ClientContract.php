9565b22a678af9b024a6fd44a173965b7772a93e
Sander Muller <github@scode.nl>
2024-04-01 14:37:28 +0200

    Update ClientContract.php

--- src/Contracts/ClientContract.php
+++ src/Contracts/ClientContract.php
@@ -2,7 +2,16 @@
 
 namespace SoftRules\PHP\Contracts;
 
+use DOMDocument;
+
 interface ClientContract
 {
-    //
+    public function firstPage(string $xml): DOMDocument;
+
+    public function updateUserInterface(string $id, string $xml): DOMDocument;
+
+    public function nextPage(string $id, string $xml): DOMDocument;
+
+    public function previousPage(string $id, string $xml): DOMDocument;
+
 }
