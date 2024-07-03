4dadb4070acc2a951f179e8c7a8f5d96e1af9ec9
SoftRules <148444307+SoftRules@users.noreply.github.com>
2024-07-03 10:22:12 +0200

    Update JS tbv logging (tijdelijk)

--- resources/js/softRules.js
+++ resources/js/softRules.js
@@ -1,4 +1,4 @@
-﻿//versie 26-06-2024
+﻿//versie 03-07-2024 
 import './rSlider.min.js';
 import { delegate } from 'tippy.js';
 import 'tippy.js/dist/tippy.css'; // optional for styling
@@ -274,14 +274,17 @@ function scriptActions(id) {
 
                 for (let i = 0; i < obj.length; i++) {
                     if (obj[i].Command === 'Hide') {
+                        console.log('Hide: ' +  $('[data-id=' + obj[i].ItemID + ']').attr('class') + ' name:' + $('[data-id=' + obj[i].ItemID + ']').attr('name'));
                         $('[data-id=' + obj[i].ItemID + ']').hide();
                         $('#' + obj[i].ItemID + '-row').hide();
                     } else if (obj[i].Command === 'Show') {
                         $('[data-id=' + obj[i].ItemID + ']').show();
                         $('#' + obj[i].ItemID + '-row').show();
                     } else if (obj[i].Command === 'Valid') {
+                        console.log('Valid: ' +  $('[data-id=' + obj[i].ItemID + ']').attr('class') + ' name:' + $('[data-id=' + obj[i].ItemID + ']').attr('name'));
                         $('[data-id=' + obj[i].ItemID + ']').attr('data-isvalid', true);
                     } else if (obj[i].Command === 'Invalid') {
+                        console.log('Invalid: ' +  $('[data-id=' + obj[i].ItemID + ']').attr('class') + ' name:' + $('[data-id=' + obj[i].ItemID + ']').attr('name'));
                         $('[data-id=' + obj[i].ItemID + ']').attr('data-isvalid', false);
                     } else if (obj[i].Command === 'Required') {
                         $('[data-id=' + obj[i].ItemID + ']').prop('required', true);
