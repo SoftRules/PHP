8ad29270c559a370adb9933f0c4b71fdfea872be
Sander Muller <github@scode.nl>
2025-01-11 09:41:28 +0100

    cleanup rector

--- rector.php
+++ rector.php
@@ -3,24 +3,16 @@
 use Rector\Arguments\Rector\ClassMethod\ArgumentAdderRector;
 use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
-use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
-use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\CodingStyle\Rector\If_\NullableCompareToNullRector;
 use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector;
-use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
-use Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector;
 use Rector\Php70\Rector\StaticCall\StaticCallOnNonStaticToInstanceCallRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
 use Rector\Php74\Rector\Ternary\ParenthesizeNestedTernaryRector;
 use Rector\Php80\Rector\Class_\StringableForToStringRector;
-use Rector\Php81\Rector\Array_\FirstClassCallableRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
 /**
@@ -36,35 +28,17 @@ return RectorConfig::configure()
         ParenthesizeNestedTernaryRector::class,
     ])
     ->withSkip([
-        AddOverrideAttributeToOverriddenMethodsRector::class,
         ArgumentAdderRector::class,
         ClosureToArrowFunctionRector::class,
         DisallowedEmptyRuleFixerRector::class,
         EncapsedStringsToSprintfRector::class,
         ExplicitBoolCompareRector::class,
-        FirstClassCallableRector::class => [
-            //
-        ],
         NullableCompareToNullRector::class,
-        ReadOnlyClassRector::class => [
-            //
-        ],
-        ReadOnlyPropertyRector::class => [
-            //
-        ],
-        RemoveNullPropertyInitializationRector::class => [
-            //
-        ],
-        RenameParamToMatchTypeRector::class,
-        RenameVariableToMatchNewTypeRector::class,
+        RemoveNullPropertyInitializationRector::class,
         RestoreDefaultNullToNullableTypePropertyRector::class,
         SeparateMultiUseImportsRector::class,
-        StaticArrowFunctionRector::class,
         StaticCallOnNonStaticToInstanceCallRector::class,
-        StaticClosureRector::class,
-        StringableForToStringRector::class => [
-            //
-        ],
+        StringableForToStringRector::class,
     ])
     ->withPaths([
         __DIR__ . '/src',
@@ -79,10 +53,7 @@ return RectorConfig::configure()
         codingStyle: true,
         typeDeclarations: true,
         privatization: true,
-        naming: false,
-        instanceOf: false,
         earlyReturn: true,
-        strictBooleans: false,
         carbon: true,
         rectorPreset: true,
     )
